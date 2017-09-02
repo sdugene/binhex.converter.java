6c1c81401653af8f20b86ff5b57a3c9792001a9f
Sebastien Dugene <sebastien.dugene@gmail.com>
2017-09-02 17:57:10 +0200

    SO > jenkins > unit Tests

--- src/main/java/com/binhex/converter/Converter.java
+++ src/main/java/com/binhex/converter/Converter.java
@@ -13,22 +13,30 @@ public class Converter
     {
         byte[] bytes;
         Formatter f = new Formatter();
-        bytes = binString.getBytes("utf-8");
-        for (byte c : bytes)
-            f.format("%02X",c);
+        try {
+            bytes = binString.getBytes("utf-8");
+            for (byte c : bytes)
+                f.format("%02X",c);
 
-        String string = f.toString().toLowerCase();
-        f.close();
-        return string;
+            return (f.toString().toLowerCase());
+        } catch(UnsupportedEncodingException e){
+            throw(e);
+        } finally {
+            f.close();
+        }
     }
 
     public static String hex2bin(String hexString) throws NumberFormatException
     {
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i < hexString.length(); i+=2) {
-            String str = hexString.substring(i, i+2);
-            output.append((char)Integer.parseInt(str, 16));
+        try {
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < hexString.length(); i+=2) {
+                String str = hexString.substring(i, i+2);
+                output.append((char)Integer.parseInt(str, 16));
+            }
+            return output.toString();
+        } catch(NumberFormatException e){
+            throw(e);
         }
-        return output.toString();
     }
 }
